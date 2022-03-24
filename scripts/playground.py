f096ba2481fa4d66373f533edd44e5850b88e637
adXerg <adxergsansoraix@gmail.com>
2022-03-24 13:26:12 +0900

    update script

--- scripts/playground.py
+++ scripts/playground.py
@@ -10,92 +10,9 @@ import torch.nn.functional as F
 import torch
 import pickle as pkl
 from utils.pydmps_torch import DMPs_discrete_torch
+from utils.networks import SegmentDMPCNN
 
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-class SegmentDMPCNN(nn.Module):
-    def __init__(self, train_param):
-        super().__init__()
-        self.train_param        = train_param
-        self.model_param        = self.train_param.model_param
-        train_param.model_param.dmp_param          = self.model_param.dmp_param
-        self.max_segments       = self.model_param.max_segments
-        self.num_position       = self.max_segments + 1
-        self.dof                = train_param.model_param.dmp_param.dof
-        self.hidden_layer_sizes = self.model_param.layer_sizes
-
-        self.conv_1             = nn.Conv2d(in_channels=self.model_param.image_dim[0], out_channels=256, kernel_size=5).to(DEVICE)
-        self.conv_2             = nn.Conv2d(in_channels=self.model_param.image_dim[0], out_channels=256, kernel_size=10).to(DEVICE)
-        self.conv_width         = nn.Conv2d(in_channels=self.model_param.image_dim[0], out_channels=256, kernel_size=(5, 49)).to(DEVICE)
-        self.conv_height        = nn.Conv2d(in_channels=self.model_param.image_dim[0], out_channels=256, kernel_size=(49, 5)).to(DEVICE)
-
-        self.dropout = nn.Dropout(p = self.model_param.dropout_prob)
-        self.tanh               = torch.nn.Tanh().to(DEVICE)
-        self.num_segments_softmax = nn.Softmax(dim = 1).to(DEVICE)
-
-        _x = torch.ones(1, self.model_param.image_dim[0], self.model_param.image_dim[1], self.model_param.image_dim[2]).to(DEVICE)
-        conv_output_size = self.forwardConv(_x).shape[1]
-        self.hidden_layer_sizes = [conv_output_size] + self.hidden_layer_sizes
-        
-        self.fc = ModuleList()
-        for idx in range(len(self.hidden_layer_sizes[:-1])):
-            self.fc.append(nn.Linear(self.hidden_layer_sizes[idx], self.hidden_layer_sizes[idx+1]).to(DEVICE))
-
-        output_num_segments_size    = 1
-        # output_y0_size              = self.dof
-        # output_goal_size            = self.dof
-        output_pos_size             = self.num_position * self.dof
-        output_w_size               = self.max_segments * self.dof * train_param.model_param.dmp_param.n_bf
-
-        self.output_num_segments    = nn.Linear(self.hidden_layer_sizes[-1], output_num_segments_size).to(DEVICE)
-        # self.output_y0              = nn.Linear(self.hidden_layer_sizes[-1], output_y0_size).to(DEVICE)
-        # self.output_goal            = nn.Linear(self.hidden_layer_sizes[-1], output_goal_size).to(DEVICE)
-        self.output_pos             = nn.Linear(self.hidden_layer_sizes[-1], output_pos_size).to(DEVICE)
-        self.output_w               = nn.Linear(self.hidden_layer_sizes[-1], output_w_size).to(DEVICE)
-
-    def forwardConv(self, x):
-        x0 = F.relu(F.max_pool2d(self.conv_1(x), 2), inplace=False)
-        x0 = flatten(x0, 1)
-
-        x1 = F.relu(F.max_pool2d(self.conv_2(x), 2), inplace=False)
-        x1 = flatten(x1, 1)
-
-        x2 = F.relu(F.max_pool2d(self.conv_width(x), 2), inplace=False)
-        x2 = flatten(x2, 1)
-
-        x3 = F.relu(F.max_pool2d(self.conv_height(x), 2), inplace=False)
-        x3 = flatten(x3, 1)
-
-        x = torch.cat([x0, x1, x2, x3], dim = 1)
-        # x = x1
-        return x.cuda()
-
-    def forward(self, x):
-        if type(x) == dict:
-            x = self.forwardConv(x['image'])
-        else:
-            x = self.forwardConv(x)
-
-        batch_s = x.shape[0]
-
-        for fc in self.fc[:-1]:
-            x = self.tanh(fc(x))
-            # x = self.tanh(fc(self.dropout(x)))
-        # x = self.fc[-1](x)
-        x = self.fc[-1](self.dropout(x))
-
-        num_segments    = self.output_num_segments(x)
-        # y0              = self.output_y0(x)
-        # goal            = self.output_goal(x)
-        pos             = self.output_pos(x).reshape(batch_s, self.num_position, self.dof)
-        w               = self.output_w(x).reshape(batch_s, self.max_segments, self.dof * train_param.model_param.dmp_param.n_bf)
-
-        y0 = pos[:, 0]
-        goals = pos[:, 1:] 
-
-        return [num_segments, y0, goals, w]
-    
-
 
 img_path = 'D:\\rllab\\scripts\\dmp\\scripts'
 train_param_path = join(img_path, 'train-model-dmp_param.pkl')
